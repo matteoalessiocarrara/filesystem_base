2a97bac6bf6b5f4d685013499659277678cc84e0
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-08-03 16:17:50 +0200

    Fixato bug e passato a c89

--- src/test.c
+++ src/test.c
@@ -17,27 +17,44 @@
  * MA 02110-1301, USA.
  */
 
-# include <stdbool.h>
 # include <malloc.h>
-# include <stdint.h>
 # include <stdio.h>
 # include <math.h>
 
 
 
-// WARNING	Queste funzioni sono qui solo per facilitare il debug. In futuro dovranno 
-//			essere spostate in una libreria condivisa
+/* WARNING Queste funzioni sono qui solo per facilitare il debug. In futuro
+ * dovranno essere spostate in una libreria condivisa */
 
 
 
-// TIPI
+/* EMULAZIONE C99 */
 
 
 
-// Lunghezza della bitmap in byte. 
-// Deve contenere un valore di 1/8 dei byte sul disco. Su un disco di 16 byte
-// per es. dovrà contenere il valore "2" (1/8*16)
-typedef uint16_t bitmap_byte_len_t;
+/* stdint.h */
+typedef unsigned char uint_least8_t;
+typedef unsigned short uint_least16_t;
+
+
+/* stdbool.h */
+# define true 1
+# define false 0
+
+typedef uint_least8_t bool;
+
+
+
+
+/* TIPI */
+
+
+
+/* Lunghezza della bitmap in byte.
+ * WARNING Assicurarsi che il tipo sia abbastanza grande per evitare overflow.
+ * Deve contenere un valore di 1/8 dei byte sul disco. Su un disco di 16 byte
+ * per es. dovrà contenere il valore "2" (1/8*16) */
+typedef uint_least16_t bitmap_byte_len_t;
 
 
 struct bitmap
@@ -45,24 +62,24 @@ struct bitmap
 		bitmap_byte_len_t	byte_len;
 		void				*content;
 		
-		// Se per es. la bitmap è di 9 bit, devono essere allocati 2 byte. Gli 
-		// ultimi 7 bit del secondo byte comunque saranno solo di riempimento 
-		// (NON DEVONO ESSERE CONSIDERATI VALIDI)
-		uint8_t			padding_bits;
+		/* Se per es. la bitmap è di 9 bit, devono essere allocati 2 byte. Gli
+		 *  ultimi 7 bit del secondo byte comunque saranno solo di riempimento
+		 * (NON DEVONO ESSERE CONSIDERATI VALIDI) */
+		uint_least8_t			padding_bits;
 };
 
 
 
-// ALTRO
+/* ALTRO */
 
 
 
 long file_get_byte_size(FILE *fp)
 {
-	long prev=ftell(fp);
+	long prev=ftell(fp), size;
 	
 	fseek(fp, 0, SEEK_END);
-	long size=ftell(fp);
+	size=ftell(fp);
 
 	fseek(fp, prev, SEEK_SET);
 	return size;
@@ -70,39 +87,40 @@ long file_get_byte_size(FILE *fp)
 
 
 
-// BITMAP
-// TODO Compressione
+/* BITMAP */
+/* TODO Compressione */
 
 
-// Offset in bit
+/* Offset in bit */
 static void __set_bit(void *buffer, size_t offset, bool enable)
 {
 	if (enable)
-		*((uint8_t*)buffer + offset/8) |= (1 << (7 - offset%8));
+		*((uint_least8_t*)buffer + offset/8) |= (1 << (7 - offset%8));
 	else
-		*((uint8_t*)buffer + offset/8) &= ~(1 << (7 - offset%8));
+		*((uint_least8_t*)buffer + offset/8) &= ~(1 << (7 - offset%8));
 }
 
 
-// Imposta lo spazio utilizzato
-// Start e offset in byte
+/* Imposta lo spazio utilizzato
+ * Start e offset in byte */
 static void __bitmap_set_used(struct bitmap *b, size_t start, size_t offset)
 {
-	for(size_t i = 0; i < offset; i++)
-		__set_bit(b->content, i, true);
+	size_t i = 0;
+	for(; i < offset; i++)
+		__set_bit(b->content, start + i, true);
 }
 
 
-// Restituisce lo spazio fisicamente usato da una struct bitmap (in byte)
+/* Restituisce lo spazio fisicamente usato da una struct bitmap (in byte) */
 static size_t __bitmap_get_size(struct bitmap *b)
 {
-	// La bitmap contiene un puntatore a content, ma va considerata la dimensione
-	// del suo contenuto, non del puntatore
+	/* La bitmap contiene un puntatore a content, ma va considerata la dimensione
+	 * del suo contenuto, non del puntatore */
 	return sizeof(b->byte_len) + sizeof(b->padding_bits) + b->byte_len;
 }
 
 
-// Restituisce la struct inizializzata
+/* Restituisce la struct inizializzata */
 static struct bitmap __bitmap_create(size_t disk_size)
 {
 	struct bitmap ret;
@@ -111,14 +129,14 @@ static struct bitmap __bitmap_create(size_t disk_size)
 	ret.padding_bits = (disk_size % 8)? 8 - (disk_size % 8) : 0;
 	ret.content = calloc(ret.byte_len, 1);
 	
-	// Va segnato lo spazio occupato dalla bitmap stessa
+	/* Va segnato lo spazio occupato dalla bitmap stessa */
 	__bitmap_set_used(&ret, 0, __bitmap_get_size(&ret));
 	
 	return ret;
 }
 
 
-// Scrive la bitmap su un disco
+/* Scrive la bitmap su un disco */
 static void __bitmap_write(struct bitmap *b, FILE *dev)
 {
 	fwrite(&(b->byte_len), sizeof(b->byte_len), 1, dev);
@@ -128,7 +146,7 @@ static void __bitmap_write(struct bitmap *b, FILE *dev)
 
 
 
-// API
+/* API */
 
 
 
@@ -142,7 +160,7 @@ void create_fs(char *dev)
 
 
 
-// TEST
+/* TEST */
 
 
 
